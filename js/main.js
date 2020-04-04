21c8140e9fda6a999e42624322e21ea3aa7780ac
goutambseervi <goutam@mirrorloops.com>
2020-04-04 13:08:07 +0530

    added animations and cookies

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     const controller = new ScrollMagic.Controller();
 
     const tween = new TimelineMax()
@@ -6,9 +7,64 @@ $(document).ready(function () {
         .add(TweenMax.to("#form-subtitle", 1, { className: "+=animated-subtitle"}), 0)
         .add(TweenMax.to("#contact-form", 1, {className: "+=custom-form"}), 0);
 
-    const scene = new ScrollMagic.Scene({triggerElement: "#contact-me", duration: 700, offset: -100})
+    new ScrollMagic.Scene({triggerElement: "#mobile-1", duration: 700, offset: -25})
+        .setClassToggle("#mobile-1", "custom-visible")
+        .addIndicators({name: "mobile 1"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#mobile-2", duration: 700, offset: -25})
+        .setClassToggle("#mobile-2", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#mobile-3", duration: 700, offset: -25})
+        .setClassToggle("#mobile-3", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#mobile-4", duration: 700, offset: -25})
+        .setClassToggle("#mobile-4", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#mobile-5", duration: 700, offset: -25})
+        .setClassToggle("#mobile-5", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#mobile-6", duration: 700, offset: -25})
+        .setClassToggle("#mobile-6", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#pricing", duration: 700, offset: -25})
+        .setClassToggle("#pricing", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#home", duration: 700, offset: -25})
+        .setClassToggle("#home", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#home-sub-1", duration: 250, offset: -25})
+        .setClassToggle("#home-sub-1", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#home-sub-2", duration: 250, offset: -25})
+        .setClassToggle("#home-sub-2", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#home-sub-3", duration: 250, offset: -25})
+        .setClassToggle("#home-sub-3", "custom-visible")
+        .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
+        .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#contact-me", duration: 700, offset: -100})
         .setTween(tween)
-        .setClassToggle(".custom-invisible", "custom-visible")
+        .setClassToggle("#contact-me", "custom-visible")
         .addIndicators({name: "tween css class"}) // add indicators (requires plugin)
         .addTo(controller);
 });

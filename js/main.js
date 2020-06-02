e9de639e013acc27d698088fe11f8b8d56ffe665
Rakesh <rabs18is@cmrit.ac.in>
2020-06-02 10:46:29 +0530

    portfolio update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-
+    $('#cta-button').click(function () {
+        document.getElementById('contact-me').scrollIntoView({ behavior: 'smooth'})
+    })
     new Typewriter('#role-text', {
-        strings: ['Developer', 'Designer'],
+        strings: ['Designer', 'Developer'],
         autoStart: true,
         loop: true
     });
@@ -11,30 +13,6 @@ $(document).ready(function () {
         .add(TweenMax.to("#form-title", 1, { className: "+=animated-title"}), 0)
         .add(TweenMax.to("#form-subtitle", 1, { className: "+=animated-subtitle"}), 0)
         .add(TweenMax.to("#contact-form", 1, {className: "+=custom-form"}), 0);
-
-    new ScrollMagic.Scene({triggerElement: "#tech-stack", duration: "150%", offset: -25})
-        .setClassToggle("#tech-stack", "custom-visible")
-        .addTo(controller);
-
-    new ScrollMagic.Scene({triggerElement: "#pricing", duration: "150%", offset: -25})
-        .setClassToggle("#pricing", "custom-visible")
-        .addTo(controller);
-
-    new ScrollMagic.Scene({triggerElement: "#home", duration: 700, offset: -25})
-        .setClassToggle("#home", "custom-visible")
-        .addTo(controller);
-
-    new ScrollMagic.Scene({triggerElement: "#home-sub-1", duration: 250, offset: -25})
-        .setClassToggle("#home-sub-1", "custom-visible")
-        .addTo(controller);
-
-    new ScrollMagic.Scene({triggerElement: "#home-sub-2", duration: 250, offset: -25})
-        .setClassToggle("#home-sub-2", "custom-visible")
-        .addTo(controller);
-
-    new ScrollMagic.Scene({triggerElement: "#home-sub-3", duration: 250, offset: -25})
-        .setClassToggle("#home-sub-3", "custom-visible")
-        .addTo(controller);
 
     new ScrollMagic.Scene({triggerElement: "#contact-me", duration: 700, offset: -100})
         .setTween(tween)

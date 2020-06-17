d24dbb7768242c5ff0b5df46e43976407549a1d5
Rakesh <rabs18is@cmrit.ac.in>
2020-06-17 12:16:43 +0530

    website update

--- js/main.js
+++ js/main.js
@@ -10,12 +10,9 @@ $(document).ready(function () {
     const controller = new ScrollMagic.Controller();
 
     const tween = new TimelineMax()
-        .add(TweenMax.to("#form-title", 1, { className: "+=animated-title"}), 0)
-        .add(TweenMax.to("#form-subtitle", 1, { className: "+=animated-subtitle"}), 0)
         .add(TweenMax.to("#contact-form", 1, {className: "+=custom-form"}), 0);
 
     new ScrollMagic.Scene({triggerElement: "#contact-me", duration: 700, offset: -100})
         .setTween(tween)
-        .setClassToggle("#contact-me", "custom-visible")
         .addTo(controller);
 });
